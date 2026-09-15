35a39d31d1b887ee21585a4a4f98defc6154c540
Louie <Louie@gmail.com>
2026-09-15 19:51:17 +0800

    reconstruct testingpy

--- TESTING.py
+++ TESTING.py
@@ -1,22 +1,13 @@
-{
-    "id" : 1,
-    "message" :{
-        "array" : [
-            "P",
-            "U",
-            "T",
-            "A",
-            "N",
-            "G",
-            "I",
-            "N",
-            "A",
-            "M",
-            "O"
-        ],
-        "hint" : "bwakanangshit ka"
+from fastapi import FastAPI
 
+app = FastAPI()
 
+@app.get("/")
+def get_data():
+    return {
+        "id": 1,
+        "message": {
+            "array": ["P", "U", "T", "A", "N", "G", "I", "N", "A", "M", "O"],
+            "hint": "bwakanangshit ka"
+        }
     }
-
-};
