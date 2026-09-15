06e726473552e04b1f160daf008ed29e49e57376
Louie <Louie@gmail.com>
2026-09-15 18:38:59 +0800

    initial commit

---
+++ TESTING.py
@@ -0,0 +1,22 @@
+{
+    "id" : 1,
+    "message" :{
+        "array" : [
+            "P",
+            "U",
+            "T",
+            "A",
+            "N",
+            "G",
+            "I",
+            "N",
+            "A",
+            "M",
+            "O"
+        ],
+        "hint" : "bwakanangshit ka"
+
+
+    }
+
+};
